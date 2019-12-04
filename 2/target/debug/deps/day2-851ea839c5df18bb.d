f53ebc363ae83d62fcefcc46279656ac5f217c09
GulkbagTheBag <gulkbag3@protonmail.com>
2019-12-04 16:40:35 +0200

    Day 2 Complete && Change from Go To Rust in day 2 : Rust

--- 2/target/debug/deps/day2-851ea839c5df18bb.d
+++
@@ -1,6 +0,0 @@
-/home/gulkbag/repos/aoc-2019/2/target/debug/deps/day2-851ea839c5df18bb.rmeta: src/main.rs src/../input.txt
-
-/home/gulkbag/repos/aoc-2019/2/target/debug/deps/day2-851ea839c5df18bb.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
--- 2/target/rls/debug/deps/day2-4413971c18ff8856.d
+++
@@ -1,6 +0,0 @@
-/home/gulkbag/repos/aoc-2019/2/target/rls/debug/deps/day2-4413971c18ff8856.rmeta: src/main.rs src/../input.txt
-
-/home/gulkbag/repos/aoc-2019/2/target/rls/debug/deps/day2-4413971c18ff8856.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
